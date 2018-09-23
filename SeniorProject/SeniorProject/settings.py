5dc25101172a940e52024dae7c97ff2e20c298d7
Kaius Reed <kmr117@case.edu>
2018-09-23 14:17:47 -0400

    Fixed initial view setup so that test homepage loads

--- SeniorProject/SeniorProject/settings.py
+++ SeniorProject/SeniorProject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'DrinkBeyondThePossible.apps.DrinkbeyondthepossibleConfig'
+    'DrinkBeyondThePossible.apps.DrinkbeyondthepossibleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
