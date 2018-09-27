7a537e05ba7d60b9ceafdf73c5951180f445dfaf
Kaius Reed <kmr117@case.edu>
2018-09-27 16:58:46 -0400

    Added search result page. Edited css and template for navigation

--- SeniorProject/DrinkBeyondThePossible/views.py
+++ SeniorProject/DrinkBeyondThePossible/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'DrinkBeyondThePossible/home.html', {})
+    context = {'activePage': 'Home'}
+    return render(request, 'DrinkBeyondThePossible/home.html', context=context)
 
 def detail(request):
-    return render(request, 'DrinkBeyondThePossible/detail.html', {})
+    context = {}
+    return render(request, 'DrinkBeyondThePossible/detail.html', context=context)
+
+def results(request):
+    drinkResults = {'Test 1', 'Test 2'}
+
+    context = {'drinkResults': drinkResults}
+    return render(request, 'DrinkBeyondThePossible/search_results.html', context=context)
