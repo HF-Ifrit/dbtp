6d0a07d45d566fd56bd964702f640e57ab63d8c7
Kaius Reed <kmr117@case.edu>
2018-09-27 14:23:26 -0400

    Added drink detail page and drink icon

--- SeniorProject/DrinkBeyondThePossible/views.py
+++ SeniorProject/DrinkBeyondThePossible/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'DrinkBeyondThePossible/home.html', {})
+
+def detail(request):
+    return render(request, 'DrinkBeyondThePossible/detail.html', {})
