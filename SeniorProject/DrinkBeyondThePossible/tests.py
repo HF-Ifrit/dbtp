2672b86635953135ac54707af1ec7f2bbbd487c0
Kaius Reed <kmr117@case.edu>
2018-12-10 15:45:16 -0500

    Added a test and requirements text file for required python modules

--- SeniorProject/DrinkBeyondThePossible/tests.py
+++ SeniorProject/DrinkBeyondThePossible/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class SearchResultsViewTest(TestCase):
+    def test_no_search_results(self):
+        """
+        If there are no valid drinks for the input search entries, the results view should load and
+        say so
+        """
+        response = self.client.post(reverse('results'), data={'ingredient': ['badIngredient']})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response.text, "No matching drinks found for ingredients: badIngredient ")
